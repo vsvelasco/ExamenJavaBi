c6204e5a1d90137479e72fc2c4370dcaa408acfc
vsvelasco <vsvelasco@gmail.com>
2023-01-04 22:06:59 -0600

    Hasta ejercicio 10

---
+++ src/pruebas_java/Ejercicio10.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pruebas_java;
+
+/**
+ *
+ * @author xm1ayy
+ */
+public class Ejercicio10 {
+    public static void main (String[]args) {
+int x = 0; 
+int y = 0; 
+while (X<5){
+y + = x;
+} x++; 
+System.out.println(y);
+}
+}
+    
+}
