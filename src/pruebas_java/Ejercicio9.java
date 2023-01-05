b09b1c8ce86afe61b20f2a30f0917f467da32b81
vsvelasco <vsvelasco@gmail.com>
2023-01-04 21:43:16 -0600

    Hasta ejercicio 9

---
+++ src/pruebas_java/Ejercicio9.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pruebas_java;
+
+/**
+ *
+ * @author xm1ayy
+ */
+public class Ejercicio9 {
+    public static void main (String[]args) {
+int j = 2; 
+System.out.println(j == 3);
+
+}    
+}
