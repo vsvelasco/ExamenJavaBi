a15d793b6051b6d9f4a908303b38671ee859c11f
vsvelasco <vsvelasco@gmail.com>
2023-01-04 21:21:47 -0600

    Hasta ejercicio 8

---
+++ src/pruebas_java/Ejercicio8.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pruebas_java;
+
+/**
+ *
+ * @author xm1ayy
+ */
+public class Ejercicio8 {
+    public static void main (String[]args) {
+ /*Código Uno*/
+int x= 0; 
+do {
+System.out.println(x);
+x++;
+} while (x<10);
+
+System.out.println("terminó primer código");
+
+/*Código Dos*/
+int y =0; 
+while (y<10) {
+System.out.println(y);
+y++;
+}
+
+System.out.println("terminó segundo código");
+}
+    
+}
